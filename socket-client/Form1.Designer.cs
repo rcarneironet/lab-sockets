71a956b1d0c6199027c1859726090375a0babf21
Ray Carneiro <ray.silva@msn.com>
2022-07-26 11:07:14 -0300

    init

---
+++ socket-client/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace socket_client
+{
+    partial class formCliente
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(281, 152);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(164, 61);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Enviar bytes";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // formCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 364);
+            this.Controls.Add(this.btnSend);
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.Name = "formCliente";
+            this.Text = "Socket Client";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSend;
+    }
+}
---
+++ socket-client/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace socket_client
+{
+
+    public partial class formCliente : Form
+    {
+        public formCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            AsynchronousClient.StartClient();
+        }
+    }
+}
